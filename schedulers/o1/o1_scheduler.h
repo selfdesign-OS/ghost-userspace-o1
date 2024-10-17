6ef757ae88a4cef0008d27e00739fe68ad2fa7d3
imjaehyeong <dlawogud517@gmail.com>
2024-10-17 10:05:33 +0900

    test: remainingtime 재설정

--- schedulers/o1/o1_scheduler.h
+++ schedulers/o1/o1_scheduler.h
@@ -46,7 +46,7 @@ struct O1Task : public Task<> {
   }
 
   void SetRemainingTime() {
-    remaining_time = absl::Nanoseconds(10*1000000); // 10ms
+    remaining_time = absl::Nanoseconds(10*500); // 10ms
   }
 
   void SetRuntimeAtLastPick() {
