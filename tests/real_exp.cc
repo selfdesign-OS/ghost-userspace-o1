ee4cdde28c4f412d558f93f72343b4413d8f218a
imjaehyeong <dlawogud517@gmail.com>
2024-10-18 17:56:40 +0900

    test:real_exp 간소화

--- tests/real_exp.cc
+++ tests/real_exp.cc
@@ -144,11 +144,11 @@ int main() {
     ghost::ScopedTime time;
     ghost::SimpleExpMany(30);
   }
-  {
-    printf("BusyExp\n");
-    ghost::ScopedTime time;
-    ghost::BusyExpRunFor(1000, absl::Milliseconds(10));
-  }
+  // {
+  //   printf("BusyExp\n");
+  //   ghost::ScopedTime time;
+  //   ghost::BusyExpRunFor(1000, absl::Milliseconds(10));
+  // }
   // {
   //   printf("TaskDeparted\n");
   //   ghost::ScopedTime time;
